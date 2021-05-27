4ca016b646f41e87f364d93c7d1c7aaef93ea32f
Sayan Das <67961536+senshiii@users.noreply.github.com>
2021-05-27 21:26:03 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBcvsZMjW33xATN4mrEY2tgjbFTH2y06d4",
-    authDomain: "picturesque-4a060.firebaseapp.com",
-    databaseURL: "https://picturesque-4a060.firebaseio.com",
-    projectId: "picturesque-4a060",
-    storageBucket: "picturesque-4a060.appspot.com",
-    messagingSenderId: "667225514922",
-    appId: "1:667225514922:web:3c06145371c639eb38c1d3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
